e5d5cf8477f771142122a2a1095919d6bce57356
deepkolos <ouyanghaozhu@oppentech.com>
2021-04-18 13:34:12 +0800

    init

---
+++ README-TPL.md
@@ -0,0 +1,37 @@
+<h2><img src="https://emojis.slackmojis.com/emojis/images/1588866973/8934/hellokittydance.gif?1588866973" alt="Hi" width="42" />欢迎来访我的Github</h2>
+
+我网名是`DeepKolos`，是一名前端开发，目前就职于奥本未来
+
+[![github](https://img.shields.io/badge/-deepkolos-%23323031?style=flat&logo=github)](https://github.com/deepkolos)
+![wechat](https://img.shields.io/badge/-deepkolos-%23323031?style=flat&logo=wechat)
+<a href="mailto:deepkolos@foxmial.com"><img src="https://img.shields.io/badge/-deepkolos@foxmial.com-%23323031?style=flat&logo=gmail" alt="foxmail" /></a>
+![profile views](https://komarev.com/ghpvc/?username=deepkolos&style=flat)
+
+#### 语言 & 工具
+
+![HTML5](https://img.shields.io/badge/-HTML5-E34C26?style=flat&logo=html5&logoColor=ffffff)
+![CSS3](https://img.shields.io/badge/-CSS3-197CBE?style=flat&logo=css3)
+![JavaScript](https://img.shields.io/badge/-JavaScript-F7DF1C?style=flat&logo=javascript&logoColor=000000&labelColor=ECD83E&color=ECD83E)
+![Preact](https://img.shields.io/badge/-Preact-20232A?logoColor=61DAFB&style=flat&logo=react)
+![Vue](https://img.shields.io/badge/-Vue-07C160?logoColor=FFFFFF&style=flat&logo=Vue.js)
+![WebGL](https://img.shields.io/badge/-WebGL-990000?style=flat&logo=WebGL&logoColor=ffffff)
+![ThreeJS](https://img.shields.io/badge/-ThreeJS-20232A?style=flat&logo=Three.js&logoColor=ffffff)\
+![VS Code](https://img.shields.io/badge/-VSCode-0066B8?style=flat&logo=visual-studio-code)
+![Git](https://img.shields.io/badge/-Git-ED5A47?style=flat&logo=git&logoColor=ffffff)
+![小程序开发工具](https://img.shields.io/badge/-小程序开发工具-07C160?style=flat&logo=WeChat&logoColor=ffffff)
+<!-- ![WebAssembly](https://img.shields.io/badge/-WebAssembly-654FF0?style=flat&logo=webassembly&logoColor=ffffff) -->
+
+<!-- 由于开发小程序，目前主要使用![Windows](https://img.shields.io/badge/-Windows-0078D6?style=flat&logo=Windows&logoColor=ffffff)，但是希望回到![Manjaro](https://img.shields.io/badge/-Manjaro-35BF5C?style=flat&logo=Manjaro&logoColor=ffffff)，用回`I3wm` -->
+
+<img alt="deepkolos stats" width="360" src="https://github-readme-stats-deepkolos.vercel.app/api?username=deepkolos&show_icons=true&include_all_commits=true"/>
+
+<details>
+
+<summary style="font-size: 18px;display: flex;align-items: center;"><img style="margin-right: 5px;" width="18" src="https://simpleicons.org/icons/github.svg"/>我的旅程</summary>
+
+<!-- <img alt="deepkolos stats" width="360" src="https://github-readme-stats-deepkolos.vercel.app/api?username=deepkolos&show_icons=true&include_all_commits=true"><br> -->
+<!-- <img alt="deepkolos's wakatime stats" width="360" src="https://github-readme-stats-deepkolos.vercel.app/api/wakatime?username=deepkolos"> -->
+
+<!-- journey -->
+
+</details>
---
+++ update.js
@@ -0,0 +1,89 @@
+const fs = require('fs');
+
+const journey = {
+  works: {
+    'three-platformize': '一个让 THREE 平台化的项目，目前已适配微信小程序，淘宝小程序',
+    'compressed-model-diff': '模型压缩后效果对比工具',
+    'hdr-prefilter-texture': '导出PMREMGenerator生成的纹理',
+    'three-sprite-player': '小程序Threejs的分块序列图播放工具，含cli工具生成分块序列图',
+    'pc-transition': 'preact transition component build with hooks',
+    'pc-swiper': 'preact-swiper build with rxjs',
+    'pc-pcm-wave': 'preact pcm wave effect component support real time state change',
+    'vc-keep-alive': '修改了keepAlive的缓存机制, 可以像APP那样前进刷新, 返回销毁',
+    'vue-swipe-directive': 'vue swipe事件的实现, 特点是lock机制, 轻松实现和滚动互斥关系',
+    'vc-popup': '一个行为标准的vue popup组件集',
+    'vc-swipe-cell': '一个模仿SwipeCellKit的vue组件',
+    'postcss-pxtocssvar': 'plugin convert px to calc(var(--base)).',
+    'hap-types': '快应用接口定义文档转的d.ts定义包',
+    'webpack-alias-sync-plugin': '把webpack alias配置同步到jsconfig.json 和 tsconfig.json',
+    app_container: {
+      desc: 'a little little framework 一个简单的SPA框架, 按照自己对SPA的交互理解制作~',
+      href: 'https://github.com/HZ-WeiBao/app_container',
+    },
+  },
+  study: {
+    'study-log-webglfundamentals': '',
+    'study-log-threejsfundamentals': '',
+  },
+  tools: {
+    awebp: 'animated webp 小工具，主要用于透明视频生成透明webp',
+    'windows-env-cli': '一个 windows 全局环境变量设置工具',
+    'move-to-types': '私有@types包小工具, 通过复制的方式',
+    'change-color': 'a color format switcher extension for vs code',
+    keyboard: '把之前的my_keyboard优化了代码, 方便转到不同的MCU, 这次用的是STM32F103C8T6',
+    my_keyboard: '我的DIY键盘MCU主控程序, 组合键支持达到正常使用~ Arduino键盘',
+    jwweb_resource: '青果教务系统 虚拟机磁盘分享~ (已破解安装) ',
+    'wechat-emoji': '微信6.5.10聊天的表情包切图 截图提取 带透明 含表情顺序文件',
+  },
+  games: {
+    SchulteGrid: '舒尔特方格-安卓',
+    shuertefangge: '舒尔特方格-Web',
+    mineSweeper: '扫雷',
+    EggSweeper: '一个扫雷改造为回忆录的东西',
+  },
+  demos: {
+    'tfjs-treeshaking-test': 'tfjs treeshaking custom module test',
+    'three-platformize-demo': 'three-platformize测试用例',
+    'three-platformize-demo-wechat': 'three-platformize 微信小程序DEMO',
+    'three-platformize-demo-taobao': 'three-platformize 淘宝小程序DEMO',
+    'simple-screen-share-webrtc': '简单的屏幕共享, 简单解决虚拟机win共享宿主linux屏幕',
+    simpleMusic: '网易云音乐高仿, 安卓大作业, 用于理解安卓组件实现, 还有资源管理方式',
+    event: '一个过渡抽象过于灵活的手势识别库(半成品)',
+    'emmet-template-engine': '一个简单的Emmet-Template-Engine, 前端模版引擎',
+    form_system: '问卷星类似的表单系统简单模仿, 支持表单编辑, 自动保存(用于学习PHP, 原生)',
+    'deepkolos.github.io': '远古时代的博客',
+  },
+};
+
+const tables = Object.keys(journey)
+  .map(type => {
+    return Object.keys(journey[type]).reduce(
+      (acc, name) => {
+        const isCfg = typeof journey[type][name] === 'object';
+        const desc = isCfg ? journey[type][name].desc : journey[type][name];
+        const href = isCfg ? journey[type][name].href : `https://github.com/deepkolos/${name}`;
+        return (
+          acc +
+          '|' +
+          [
+            `[${name}](${href})`,
+            desc,
+            `<img alt="Stars" src="https://img.shields.io/github/stars/deepkolos/${name}?style=flat&labelColor=373f51&color=4FC08D" />`,
+            `<img alt="Stars" src="https://img.shields.io/github/forks/deepkolos/${name}?style=flat&labelColor=373f51&color=4FC08D" />`,
+          ].join('|') +
+          '|\r\n'
+        );
+      },
+      `
+### ${type[0].toUpperCase() + type.slice(1)}
+
+|📦 Projects|📃 Description|⭐ Stars|📚 Forks|
+|-----------|--------------|---------|--------|
+`,
+    );
+  })
+  .join('\n');
+
+// fs.writeFileSync('./journey.md', tables);
+const tpl = fs.readFileSync('./README-TPL.md', { encoding: 'utf8' });
+fs.writeFileSync('./README.md', tpl.replace('<!-- journey -->', tables));
